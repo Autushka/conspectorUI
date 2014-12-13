a55abf25fcd99f8fa33874b5f1b5ce421be4ced8
akrfranko <franko@akr.club>
2014-12-12 20:22:21 -0500

    installed the load-grunt-tasks npm module to automagically load grunt plugins into Gruntfile

--- conspectorUI/src/main/webapp/Gruntfile.js
+++ conspectorUI/src/main/webapp/Gruntfile.js
@@ -23,8 +23,7 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('load-grunt-tasks');
 
   grunt.registerTask('default', ['concat', 'uglify']);
 
