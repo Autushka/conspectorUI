e34f60019b6ba9bd0cd92464d834902aac29266f
Chelny <lamajeste@gmail.com>
2015-02-04 16:55:10 -0400

    - units page

--- conspectorUI/src/main/webapp/apps/conspector/components/units/controllers/unitsListController.js
+++ conspectorUI/src/main/webapp/apps/conspector/components/units/controllers/unitsListController.js
@@ -1,169 +1,169 @@
-// viewControllers.controller('unitsListView', ['$scope', '$state', 'servicesProvider', 'historyProvider', 'apiProvider',
-// 	function($scope, $state, servicesProvider, historyProvider, apiProvider) {
-// 		historyProvider.removeHistory();// because current view doesn't have a back button
+viewControllers.controller('unitsListView', ['$scope', '$state', 'servicesProvider', 'historyProvider', 'apiProvider',
+	function($scope, $state, servicesProvider, historyProvider, apiProvider) {
+		historyProvider.removeHistory();// because current view doesn't have a back button
 
-// 		var sCurrentRole = cacheProvider.oUserProfile.sCurrentRole;
-// 		$scope.bDisplayAddButton = rolesSettings.getRolesSettingsForEntityAndOperation({
-// 			sRole: sCurrentRole,
-// 			sEntityName: "oUnit",
-// 			sOperation: "bCreate"
-// 		});
+		var sCurrentRole = cacheProvider.oUserProfile.sCurrentRole;
+		$scope.bDisplayAddButton = rolesSettings.getRolesSettingsForEntityAndOperation({
+			sRole: sCurrentRole,
+			sEntityName: "oUnit",
+			sOperation: "bCreate"
+		});
 
-// 		$scope.bDisplayEditButtons = rolesSettings.getRolesSettingsForEntityAndOperation({
-// 			sRole: sCurrentRole,
-// 			sEntityName: "oUnit",
-// 			sOperation: "bUpdate"
-// 		});
+		$scope.bDisplayEditButtons = rolesSettings.getRolesSettingsForEntityAndOperation({
+			sRole: sCurrentRole,
+			sEntityName: "oUnit",
+			sOperation: "bUpdate"
+		});
 
-// 		$rootScope.sCurrentStateName = $state.current.name; // for backNavigation	
-// 		$rootScope.oStateParams = {}; // for backNavigation			
+		$rootScope.sCurrentStateName = $state.current.name; // for backNavigation	
+		$rootScope.oStateParams = {}; // for backNavigation			
 
-// 		var oUnitsListData = {
-// 			aData: []
-// 		};
+		var oUnitsListData = {
+			aData: []
+		};
 
-// 		var oTableStatusFromCache = cacheProvider.getTableStatusFromCache({
-// 			sTableName: "unitsList",
-// 			sStateName: $rootScope.sCurrentStateName,
-// 		});
+		var oTableStatusFromCache = cacheProvider.getTableStatusFromCache({
+			sTableName: "unitsList",
+			sStateName: $rootScope.sCurrentStateName,
+		});
 
-// 		var oInitialSortingForUnitsList = {};
-// 		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.oSorting, {})) {
-// 			oInitialSortingForUnitsList = angular.copy(oTableStatusFromCache.oSorting);
-// 		}
-// 		var oInitialFilterForUnitsList = {};
-// 		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.oFilter, {})) {
-// 			oInitialFilterForUnitsList = angular.copy(oTableStatusFromCache.oFilter);
-// 		}
-// 		var oInitialGroupsSettingsForUnitsList = [];
-// 		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.aGroups, [])) {
-// 			oInitialGroupsSettingsForUnitsList = angular.copy(oTableStatusFromCache.aGroups);
-// 		}
+		var oInitialSortingForUnitsList = {};
+		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.oSorting, {})) {
+			oInitialSortingForUnitsList = angular.copy(oTableStatusFromCache.oSorting);
+		}
+		var oInitialFilterForUnitsList = {};
+		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.oFilter, {})) {
+			oInitialFilterForUnitsList = angular.copy(oTableStatusFromCache.oFilter);
+		}
+		var oInitialGroupsSettingsForUnitsList = [];
+		if (oTableStatusFromCache && !angular.equals(oTableStatusFromCache.aGroups, [])) {
+			oInitialGroupsSettingsForUnitsList = angular.copy(oTableStatusFromCache.aGroups);
+		}
 
-// 		$scope.tableParams = servicesProvider.createNgTable({
-// 			oInitialDataArrayWrapper: oUnitsListData,
-// 			sDisplayedDataArrayName: "aDisplayedUnits",
-// 			oInitialSorting: oInitialSortingForUnitsList,
-// 			oInitialFilter: oInitialFilterForUnitsList,
-// 			aInitialGroupsSettings: oInitialGroupsSettingsForUnitsList,
-// 			sGroupBy: "sProjectPhase",
-// 			sGroupsSortingAttribue: "_sortingSequence" //for default groups sorting
-// 		});	
+		$scope.tableParams = servicesProvider.createNgTable({
+			oInitialDataArrayWrapper: oUnitsListData,
+			sDisplayedDataArrayName: "aDisplayedUnits",
+			oInitialSorting: oInitialSortingForUnitsList,
+			oInitialFilter: oInitialFilterForUnitsList,
+			aInitialGroupsSettings: oInitialGroupsSettingsForUnitsList,
+			sGroupBy: "sProjectPhase",
+			sGroupsSortingAttribue: "_sortingSequence" //for default groups sorting
+		});	
 
-// 		var onUnitsLoaded = function(aData) {
-// 			// var sProjectName = "";
-// 			// var sPhaseName = "";
-// 			// var bMatchFound = false;
-// 			// for (var i = 0; i < aData.length; i++) {
+		var onUnitsLoaded = function(aData) {
+			// var sProjectName = "";
+			// var sPhaseName = "";
+			// var bMatchFound = false;
+			// for (var i = 0; i < aData.length; i++) {
 
-// 			// 	if (aData[i].PhaseDetails.results.length) {
-// 			// 		for (var j = 0; j < aData[i].PhaseDetails.results.length; j++) {
-// 			// 			aData[i].PhaseDetails.results[j]._sortingSequence = aData[i].PhaseDetails.results[j].GeneralAttributes.SortingSequence;
-// 			// 		}
-// 			// 		aData[i].PhaseDetails.results = $filter('orderBy')(aData[i].PhaseDetails.results, ["_sortingSequence"]);
+			// 	if (aData[i].PhaseDetails.results.length) {
+			// 		for (var j = 0; j < aData[i].PhaseDetails.results.length; j++) {
+			// 			aData[i].PhaseDetails.results[j]._sortingSequence = aData[i].PhaseDetails.results[j].GeneralAttributes.SortingSequence;
+			// 		}
+			// 		aData[i].PhaseDetails.results = $filter('orderBy')(aData[i].PhaseDetails.results, ["_sortingSequence"]);
 
-// 			// 		for (var j = 0; j < aData[i].PhaseDetails.results.length; j++) {
-// 			// 			bMatchFound = false;
-// 			// 			for (var k = 0; k < cacheProvider.oUserProfile.aGloballySelectedPhasesGuids.length; k++) {
-// 			// 				if (aData[i].PhaseDetails.results[j].Guid === cacheProvider.oUserProfile.aGloballySelectedPhasesGuids[k]) {
-// 			// 					bMatchFound = true;
-// 			// 					break;
-// 			// 				}
-// 			// 			}
-// 			// 			if (!bMatchFound) {
-// 			// 				continue;
-// 			// 			}
+			// 		for (var j = 0; j < aData[i].PhaseDetails.results.length; j++) {
+			// 			bMatchFound = false;
+			// 			for (var k = 0; k < cacheProvider.oUserProfile.aGloballySelectedPhasesGuids.length; k++) {
+			// 				if (aData[i].PhaseDetails.results[j].Guid === cacheProvider.oUserProfile.aGloballySelectedPhasesGuids[k]) {
+			// 					bMatchFound = true;
+			// 					break;
+			// 				}
+			// 			}
+			// 			if (!bMatchFound) {
+			// 				continue;
+			// 			}
 
-// 			// 			sProjectName = $translate.use() === "en" ? aData[i].PhaseDetails.results[j].ProjectDetails.NameEN : aData[i].PhaseDetails.results[j].ProjectDetails.NameFR;
-// 			// 			if (!sProjectName) {
-// 			// 				sProjectName = aData[i].PhaseDetails.results[j].ProjectDetails.NameEN;
-// 			// 			}
-// 			// 			sPhaseName = $translate.use() === "en" ? aData[i].PhaseDetails.results[j].NameEN : aData[i].PhaseDetails.results[j].NameFR;
-// 			// 			if (!sPhaseName) {
-// 			// 				sPhaseName = aData[i].PhaseDetails.results[j].NameEN;
-// 			// 			}
+			// 			sProjectName = $translate.use() === "en" ? aData[i].PhaseDetails.results[j].ProjectDetails.NameEN : aData[i].PhaseDetails.results[j].ProjectDetails.NameFR;
+			// 			if (!sProjectName) {
+			// 				sProjectName = aData[i].PhaseDetails.results[j].ProjectDetails.NameEN;
+			// 			}
+			// 			sPhaseName = $translate.use() === "en" ? aData[i].PhaseDetails.results[j].NameEN : aData[i].PhaseDetails.results[j].NameFR;
+			// 			if (!sPhaseName) {
+			// 				sPhaseName = aData[i].PhaseDetails.results[j].NameEN;
+			// 			}
 
-// 			// 			oUnitsListData.aData.push({
-// 			// 				sUnitName: aData[i].Name,
-// 			// 				sPhone: aData[i].MainPhone,
-// 			// 				sEmail: aData[i].Email,
-// 			// 				_guid: aData[i].Guid,
-// 			// 				sTags: aData[i].DescriptionTags,
-// 			// 				sProjectPhase: sProjectName + " - " + sPhaseName,
-// 			// 				_sortingSequence: aData[i].PhaseDetails.results[j]._sortingSequence, //for default groups sorting
-// 			// 			});
-// 			// 		}
-// 			// 	} else {
-// 			// 		oUnitsListData.aData.push({
-// 			// 			sUnitName: aData[i].Name,
-// 			// 			sPhone: aData[i].MainPhone,
-// 			// 			sEmail: aData[i].Email,
-// 			// 			_guid: aData[i].Guid,
-// 			// 			sTags: aData[i].DescriptionTags,
-// 			// 			sProjectPhase: "Not Assigned", // TODO should be translatable...
-// 			// 			_sortingSequence: -1, //for default groups sorting
-// 			// 		});
-// 			// 	}
+			// 			oUnitsListData.aData.push({
+			// 				sUnitName: aData[i].Name,
+			// 				sPhone: aData[i].MainPhone,
+			// 				sEmail: aData[i].Email,
+			// 				_guid: aData[i].Guid,
+			// 				sTags: aData[i].DescriptionTags,
+			// 				sProjectPhase: sProjectName + " - " + sPhaseName,
+			// 				_sortingSequence: aData[i].PhaseDetails.results[j]._sortingSequence, //for default groups sorting
+			// 			});
+			// 		}
+			// 	} else {
+			// 		oUnitsListData.aData.push({
+			// 			sUnitName: aData[i].Name,
+			// 			sPhone: aData[i].MainPhone,
+			// 			sEmail: aData[i].Email,
+			// 			_guid: aData[i].Guid,
+			// 			sTags: aData[i].DescriptionTags,
+			// 			sProjectPhase: "Not Assigned", // TODO should be translatable...
+			// 			_sortingSequence: -1, //for default groups sorting
+			// 		});
+			// 	}
 
-// 			// }
-// 			$scope.tableParams.reload();
-// 		};
+			// }
+			$scope.tableParams.reload();
+		};
 
-// 		var loadUnits = function() {
-// 			oUnitsListData.aData = [];
-// 			apiProvider.getUnits({
-// 				bShowSpinner: true,
-// 				onSuccess: onUnitsLoaded
-// 			});
-// 		};
+		var loadUnits = function() {
+			oUnitsListData.aData = [];
+			apiProvider.getUnits({
+				bShowSpinner: true,
+				onSuccess: onUnitsLoaded
+			});
+		};
 
-// 		loadUnits(); //load Units
+		loadUnits(); //load Units
 
-// 		$scope.onDisplay = function(oUnit) {
-// 			$state.go('app.UnitDetailsWrapper.UnitDetails', {
-// 				sMode: "display",
-// 				sUnitGuid: oUnit._guid,
-// 			});
-// 		};
+		$scope.onDisplay = function(oUnit) {
+			$state.go('app.UnitDetailsWrapper.UnitDetails', {
+				sMode: "display",
+				sUnitGuid: oUnit._guid,
+			});
+		};
 
-// 		$scope.onEdit = function(oUnit) {
-// 			$state.go('app.UnitDetailsWrapper.UnitDetails', {
-// 				sMode: "edit",
-// 				sUnitGuid: oUnit._guid,
-// 			});
-// 		};
+		$scope.onEdit = function(oUnit) {
+			$state.go('app.UnitDetailsWrapper.UnitDetails', {
+				sMode: "edit",
+				sUnitGuid: oUnit._guid,
+			});
+		};
 
-// 		$scope.onAddNew = function() {
-// 			$state.go('app.UnitDetailsWrapper.UnitDetails', {
-// 				sMode: "create",
-// 				sUnitGuid: "",
-// 			});
-// 		};
-// 		$scope.$on('globalUserPhasesHaveBeenChanged', function(oParameters) {
-// 			loadUnits();
-// 		});
+		$scope.onAddNew = function() {
+			$state.go('app.UnitDetailsWrapper.UnitDetails', {
+				sMode: "create",
+				sUnitGuid: "",
+			});
+		};
+		$scope.$on('globalUserPhasesHaveBeenChanged', function(oParameters) {
+			loadUnits();
+		});
 
-// 		$scope.$on('accountsShouldBeRefreshed', function(oParameters) {
-// 			loadUnits();
-// 		});
+		$scope.$on('accountsShouldBeRefreshed', function(oParameters) {
+			loadUnits();
+		});
 
-// 		$scope.$on("$destroy", function() {
-// 			if (historyProvider.getPreviousStateName() === $rootScope.sCurrentStateName) { //current state was already put to the history in the parent views
-// 				return;
-// 			}
+		$scope.$on("$destroy", function() {
+			if (historyProvider.getPreviousStateName() === $rootScope.sCurrentStateName) { //current state was already put to the history in the parent views
+				return;
+			}
 
-// 			historyProvider.addStateToHistory({
-// 				sStateName: $rootScope.sCurrentStateName,
-// 				oStateParams: $rootScope.oStateParams
-// 			});
+			historyProvider.addStateToHistory({
+				sStateName: $rootScope.sCurrentStateName,
+				oStateParams: $rootScope.oStateParams
+			});
 
-// 			cacheProvider.putTableStatusToCache({
-// 				sTableName: "unitsList",
-// 				sStateName: $rootScope.sCurrentStateName,
-// 				aGroups: $scope.tableParams.settings().$scope.$groups,
-// 				oFilter: $scope.tableParams.$params.filter,
-// 				oSorting: $scope.tableParams.$params.sorting,
-// 			});
-// 		});		
-// 	}
-// ]);
+			cacheProvider.putTableStatusToCache({
+				sTableName: "unitsList",
+				sStateName: $rootScope.sCurrentStateName,
+				aGroups: $scope.tableParams.settings().$scope.$groups,
+				oFilter: $scope.tableParams.$params.filter,
+				oSorting: $scope.tableParams.$params.sorting,
+			});
+		});		
+	}
+]);
