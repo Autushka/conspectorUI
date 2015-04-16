6372abcc0f7e2ae4f98a04cbd9d9616da58b383f
aleh.autushka <aleh@conspector.com>
2015-04-16 12:27:14 -0400

    - adding pubnub initialization to hybridApp; - updating index.html;

--- conspectorUI/src/main/webapp/apps/conspector/js/constantsHybridApp.js
+++ conspectorUI/src/main/webapp/apps/conspector/js/constantsHybridApp.js
@@ -1,5 +1,11 @@
 var oGlobalConstants = {sServicePath: "http://app.conspector.com/odata.svc/"};
 
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-ca905ca3-be6e-4a5b-96cd-49dc2312a4e6',
+    subscribe_key: 'sub-c-8b324682-73df-11e3-9291-02ee2ddab7fe'
+});
+
+
 app.factory('CONSTANTS', [function() {
 	return {
 		messageDisplayTime: 5000,
